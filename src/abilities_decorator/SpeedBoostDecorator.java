2eeede9f9a6ab86083ee56e2340263269b25b7e8
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2024-09-24 23:36:31 +0300

    ..

--- src/abilities_decorator/SpeedBoostDecorator.java
+++ src/abilities_decorator/SpeedBoostDecorator.java
@@ -3,17 +3,22 @@ package abilities_decorator;
 // Decorator - Adds Speed ability for dodge
 public class SpeedBoostDecorator implements PlayerAbility {
     private PlayerAbility playerAbility;
+    private Double chance;
 
-    public SpeedBoostDecorator(PlayerAbility playerAbility) {
+    public SpeedBoostDecorator(PlayerAbility playerAbility, Double chance) {
         this.playerAbility = playerAbility;
+        this.chance=chance;
     }
 
     @Override
     public int modifyDefense(int damage) {
-        return playerAbility.modifyDefense(damage); 
+        if (tryDodge()) {
+            return 0;
+        }
+        return playerAbility.modifyDefense(damage);
     }
 
     public boolean tryDodge() {
-        return Math.random() < 0.75; 
+        return Math.random() < chance; 
     }
 }
